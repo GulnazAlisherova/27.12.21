4927cafafed2a0dbc90de4eb01037ccf7ec8d7b5
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-27 14:22:37 +0600

    added calculateSquareArea

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 //cookdDinner
 //finishMathHomework
 
-function callFriend(friendsName) {
-  console.log("Pick up the phone");
-  console.log("Type in the number");
-  console.log("Press call");
-  console.log("Wait for the answer");
-}
+// function callFriend(friendsName = "Bakyt") {  
+//   console.log("Pick up the phone");
+//   console.log("Type in the number " + friendsName);
+//   console.log("Press call");
+//   console.log("Wait for the answer");
+// }
 
-callFriend("Bakyt");
-callFriend("Akjol");
-callFriend("Seitek");
+// callFriend("Bakyt");
+// callFriend("Akjol");
+// callFriend("Seitek");
+// callFriend();
+
+
+function calculateSquareArea(a, b) {
+  console.log(a * b);
+}

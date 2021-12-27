986c6a445a6b63937239b48ebcfe3d2ca91f6abc
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-27 14:12:49 +0600

    added

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-
 //verysimplefunction
 //playCards
 //cookdDinner
 //finishMathHomework
 
 function callFriend() {
-  console.log('Pick up the phone');
-  console.log('Type in the number');
-  console.log('Press call');
-  console.log('Wait for the answer');
+  console.log("Pick up the phone");
+  console.log("Type in the number");
+  console.log("Press call");
+  console.log("Wait for the answer");
 }
+
+callFriend();
+callFriend();
+callFriend();

fa2f93d83f5ea0cc21418066d5bfa5dcee45c109
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-27 14:10:17 +0600

    added 3 consoles

--- script.js
+++ script.js
@@ -5,5 +5,8 @@
 //finishMathHomework
 
 function callFriend() {
-  console.log('Pick up the phone')
+  console.log('Pick up the phone');
+  console.log('Type in the number');
+  console.log('Press call');
+  console.log('Wait for the answer');
 }
